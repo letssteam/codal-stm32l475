1970c74be9e0c09b9e6b78c4074c0c0e3e840bdf
Sébastien NEDJAR <nedseb@goldaire.com>
2018-07-11 09:18:33 +0200

    Add timer

---
+++ inc/stm32l4xxTimer.h
@@ -0,0 +1,23 @@
+#ifndef CODAL_STM32L4XX_TIMER_H
+#define CODAL_STM32L4XX_TIMER_H
+
+#include "codal-core/inc/types/Event.h"
+#include "codal-core/inc/driver-models/Timer.h"
+
+namespace codal
+{
+class STM32L4xxTimer : public codal::Timer
+{
+    uint32_t prev;
+public:
+    TIM_HandleTypeDef TimHandle;
+    
+    STM32L4xxTimer();
+    static STM32L4xxTimer *instance;
+    void init();
+    virtual void triggerIn(CODAL_TIMESTAMP t);
+    virtual void syncRequest();
+};
+} // namespace codal
+
+#endif
